74f75cc27704e8416c5b8b7e969d69b4f5bbcf31
Sean Thomas <spthomas127@gmail.com>
2022-06-02 20:14:51 -0400

    Removed commented code

--- get_data.py
+++ get_data.py
@@ -4,6 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import sqlite3
 import json
 
+
 class GetData:
     def __init__(self):
         self.adj_dict = {}
@@ -19,7 +20,6 @@ class GetData:
         self.songs = []
         self.conn = sqlite3.connect('adj_list.db')
         self.c = self.conn.cursor()
-
 
     def get_coldplay_relations(self):
         self.c.execute("""CREATE TABLE IF NOT EXISTS adj_list (
@@ -47,81 +47,86 @@ class GetData:
                 tracks.extend(results['items'])
 
         for track in tracks:
-            if 'Remix' not in track['name']:
-                if len(track['artists']) > 1:
-                    self.artists.extend(track['artists'])
-                if '(feat.' in track['name']:
-                    name = track['name']
-                    words = name.split('(feat.')
-                    name = ' '.join(words[2:])[:-1]
-                    if name not in self.artist_names:
-                        self.artist_names.append(name)
-                elif '(with' in track['name']:
-                    name = track['name']
-                    words = name.split('(with')
-                    name = ' '.join(words[2:])[:-1]
-                    if name not in self.artist_names:
-                        self.artist_names.append(name)
+            if len(track['artists']) > 1:
+                self.artists.extend(track['artists'])
+            if '(feat.' in track['name']:
+                name = track['name']
+                words = name.split('(feat.')
+                name = ' '.join(words[2:])[:-1]
+                if name not in self.artist_names:
+                    self.artist_names.append(name)
+            elif '(with' in track['name']:
+                name = track['name']
+                words = name.split('(with')
+                name = ' '.join(words[2:])[:-1]
+                if name not in self.artist_names:
+                    self.artist_names.append(name)
 
         for artist in self.artists:
             if artist['name'] not in self.artist_names and artist['name'] != 'Coldplay':
                 self.artist_names.append(artist['name'])
 
-        # self.adj_dict.update({'Coldplay': self.artist_names})
         blob = json.dumps(self.artist_names)
-        self.c.execute("INSERT INTO adj_list VALUES ('Coldplay', ?)", [blob])
-        self.conn.commit()
+        self.c.execute("SELECT * FROM adj_list WHERE artist=?", ('Coldplay',))
+        data = self.c.fetchone()
+        if not data:
+            self.c.execute("INSERT INTO adj_list VALUES ('Coldplay', ?)", [blob])
+            self.conn.commit()
 
     def add_layer(self):
         print("Adding layer")
         for artist in self.artist_names:
             try:
                 result = self.spotify.search(artist, type='artist')
-            except:
-                continue
-            try:
                 current_artist = result['artists']['items'][0]
-            except:
+            except spotipy.exceptions.SpotifyException:
                 continue
-            artist_uri = current_artist['uri']
+            followers = current_artist['followers']['total']
+            if followers is not None and followers > 15000:
+                artist_uri = current_artist['uri']
 
-            results = self.spotify.artist_albums(artist_uri, album_type='album,single')
-            albums = results['items']
-
-            while results['next']:
-                results = self.spotify.next(results)
-                albums.extend(results['items'])
-
-            tracks = []
-            for album in albums:
-                results = self.spotify.album_tracks(album['external_urls']['spotify'])
-                album_tracks = results['items']
-                tracks.extend(album_tracks)
+                results = self.spotify.artist_albums(artist_uri, album_type='album,single')
+                albums = results['items']
 
                 while results['next']:
                     results = self.spotify.next(results)
-                    tracks.extend(results['items'])
+                    albums.extend(results['items'])
 
-            self.artists = []
-            self.artist_names = []
-            for track in tracks:
-                if len(track['artists']) > 1:
-                    if 'Remix' not in track['name']:
+                tracks = []
+                for album in albums:
+                    results = self.spotify.album_tracks(album['external_urls']['spotify'])
+                    album_tracks = results['items']
+                    tracks.extend(album_tracks)
+
+                    while results['next']:
+                        results = self.spotify.next(results)
+                        tracks.extend(results['items'])
+
+                self.artists = []
+                self.artist_names = []
+                for track in tracks:
+                    if len(track['artists']) > 1:
                         self.artists.extend(track['artists'])
-                if 'feat' in track['name'] or '(with' in track['name']:
-                    name = track['name']
-                    words = name.split()
-                    name = ' '.join(words[2:])[:-1]
+                    if 'feat' in track['name'] or '(with' in track['name']:
+                        name = track['name']
+                        words = name.split()
+                        name = ' '.join(words[2:])[:-1]
 
-            for a in self.artists:
-                if a['name'] not in self.artist_names and a['name'] != artist:
-                    self.artist_names.append(a['name'])
+                for a in self.artists:
+                    if a['name'] not in self.artist_names and a['name'] != artist:
+                        current_artist = self.spotify.artist(a['uri'])
+                        followers = current_artist['followers']['total']
+                        if followers is not None and followers > 15000:
+                            self.artist_names.append(a['name'])
 
-            # self.adj_dict.update({artist: self.artist_names})
-            blob = json.dumps(self.artist_names)
-            self.c.execute("INSERT INTO adj_list VALUES (?, ?)", [artist, blob])
-            self.conn.commit()
-
+                # self.adj_dict.update({artist: self.artist_names})
+                blob = json.dumps(self.artist_names)
+                self.c.execute("SELECT * FROM adj_list WHERE artist=?", (artist,))
+                data = self.c.fetchone()
+                if not data:
+                    self.c.execute("INSERT INTO adj_list VALUES (?, ?)", [artist, blob])
+                    self.conn.commit()
+                    print("added")
 
     def bfs(self):
         edge_to = {}
@@ -163,7 +168,7 @@ class GetData:
                         marked[vert] = True
                         dist[vert] = dist[v] + 1
 
-        v = 'Juice WRLD'
+        v = 'U2'
         chosen_artist = v
         if v in marked.keys() and marked[v] is True:
             self.path.append(v)
@@ -178,70 +183,5 @@ class GetData:
             print(f"Coldplay is connected to {chosen_artist}!")
             self.path.reverse()
 
-            for i in range(len(self.path) - 1):
-                result = self.spotify.search(self.path[i] + " " + self.path[i + 1], type='track')
-                for j in range(5):
-                    if 'Remix' not in result['tracks']['items'][i]['name']:
-                        song = result['tracks']['items'][j]['name']
-                        self.songs.append(song)
-                        break
             print(self.path)
             print(self.songs)
-
-
-    # def bfs(self):
-    #     edge_to = {}
-    #     dist = {}
-    #     marked = {}
-    #     queue = []
-    #     print(self.adj_dict)
-    #     print("Initializing breadth-first-search")
-    #     for node in self.adj_dict.keys():
-    #         edge_to[node] = None
-    #         marked[node] = False
-    #         dist[node] = -1
-    #         for child in self.adj_dict[node]:
-    #             edge_to[child] = None
-    #             marked[child] = False
-    #             dist[child] = -1
-    #
-    #     queue.append('Coldplay')
-    #     marked['Coldplay'] = True
-    #
-    #     print("Beginning BFS")
-    #     while queue:
-    #         v = queue.pop(0)
-    #         if v in self.adj_dict:
-    #             for vert in self.adj_dict[v]:
-    #                 if marked[vert]:
-    #                     continue
-    #                 else:
-    #                     queue.append(vert)
-    #                     edge_to[vert] = v
-    #                     marked[vert] = True
-    #                     dist[vert] = dist[v] + 1
-    #
-    #     v = 'JAY-Z'
-    #     chosen_artist = v
-    #     if v in marked.keys() and marked[v] is True:
-    #         self.path.append(v)
-    #         print("Creating path")
-    #         while edge_to[v]:
-    #             self.path.append(edge_to[v])
-    #             v = edge_to[v]
-    #     else:
-    #         print("No path")
-    #
-    #     if self.path:
-    #         print(f"Coldplay is connected to {chosen_artist}!")
-    #         self.path.reverse()
-    #
-    #         for i in range(len(self.path) - 1):
-    #             result = self.spotify.search(self.path[i] + " " + self.path[i + 1], type='track')
-    #             for j in range(5):
-    #                 if 'Remix' not in result['tracks']['items'][i]['name']:
-    #                     song = result['tracks']['items'][j]['name']
-    #                     self.songs.append(song)
-    #                     break
-    #         print(self.path)
-    #         print(self.songs)
